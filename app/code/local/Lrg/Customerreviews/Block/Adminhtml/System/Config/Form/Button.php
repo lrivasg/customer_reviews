4476e32ad7cbcbb745da404ab715b6c71e9073af
Luis Rivas <lrivasg.8@gmail.com>
2017-04-05 01:48:17 +0200

    Add button to execute send reminder function

---
+++ app/code/local/Lrg/Customerreviews/Block/Adminhtml/System/Config/Form/Button.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * 
+ * @category	Lrg 
+ * @package     Lrg_Customerreviews
+ * @author      Luis Rivas <lrivasg.8@gmail.com>
+ */
+class Lrg_Customerreviews_Block_Adminhtml_System_Config_Form_Button extends Mage_Adminhtml_Block_System_Config_Form_Field
+{
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->setTemplate('customerreviews/run.phtml');
+    }
+    public function getAjaxUrl()
+    {
+        $data = $this->getRequest()->getPost();
+        if ($data['send']) {
+            Mage::dispatchEvent('send_reminders');
+
+        }
+        
+    }
+    
+    protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
+    {  
+        return $this->_toHtml();
+    }     
+        
+    public function getButtonHtml(){
+        $button = $this->getLayout()->createBlock('adminhtml/widget_button')
+            ->setData(array(
+                'id'        => 'sendreminders_button',
+                'label'     => $this->helper('customerreviews')->__('Run now'),
+                'onclick'   => 'javascript:sendreminders(); return false;'
+            ));
+        return $button->toHtml();
+    }
+    
+}
